b57d540aac1eb8526994ee0e06f3258d9be6c2c5
satish gaware <satishgaware99@Gmail.com>
2023-06-24 12:08:44 +0530

    first commit of project

---
+++ src/git_progect_1/test_1.java
@@ -0,0 +1,11 @@
+package git_progect_1;
+
+public class test_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("satish gaware");
+	}
+
+}
